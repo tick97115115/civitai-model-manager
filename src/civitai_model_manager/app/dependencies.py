2021ceebcaae58d6b340ef7f5513ad50967de95c
Pyon~ Pyon~~ <32931796+tick97115115@users.noreply.github.com>
2025-04-14 15:07:12 +0800

    refactored startup script into FastAPI Deps

--- src/civitai_model_manager/app/dependencies.py
+++ src/civitai_model_manager/app/dependencies.py
@@ -1,17 +1,222 @@
 from fastapi import Depends
 from sqlmodel import create_engine, Session, SQLModel
+from sqlalchemy import Engine
 from contextlib import contextmanager
 from typing import Annotated
 
-from civitai_model_manager.startup import settings
-from .db.civitai_table import CivitAI_ModelId
-from .db.gopeed_table import ModelVersionGopeedTask
+from pathlib import Path
 
-engine = create_engine(settings.db_uri)
-SQLModel.metadata.create_all(engine)
+from anyio import open_file
+from pydantic_settings import BaseSettings
+from pydantic import Field
+import httpx
+from os.path import join, dirname, exists
+from urllib.parse import urljoin
+import json
+from gospeed_api.index import GospeedAPI
+
+# router = APIRouter()
+
+# https://fastapi.tiangolo.com/tutorial/handling-errors/?h=exce#install-custom-exception-handlers
+
+class DatabaseNotFound(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+# @app.exception_handler(DatabaseNotFound)
+# async def database_not_found_exception_handler(request: Request, exc: DatabaseNotFound):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": exc.msg},
+#     )
+
+class ModelsFolderNotFound(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+# @app.exception_handler(LoraFolderNotFound)
+# async def lora_folder_not_found_exception_handler(request: Request, exc: LoraFolderNotFound):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": exc.msg},
+#     )
+
+class GopeedServiceNotFound(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+    
+# @app.exception_handler(GopeedServiceNotFound)
+# async def gopeed_service_not_found_exception_handler(request: Request, exc: GopeedServiceNotFound):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": exc.msg},
+#     )
+
+class GopeedServiceNotWorking(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+# @app.exception_handler(GopeedServiceNotWorking)
+# async def gopeed_service_not_working_exception_handler(request: Request, exc: GopeedServiceNotWorking):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": exc.msg},
+#     )
+
+# @app.exception_handler(LoadSettingsError)
+# async def load_settings_exception_handler(request: Request, exc: LoadSettingsError):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": exc.msg},
+#     )
+
+class SettingsJsonFileInvalideException(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+settings_file = Path(__file__).parent / 'settings.json'
+sqlite_file = join(dirname(__file__), 'db.sqlite3')
+
+class SettingsJsonModel(BaseSettings):
+    db_uri: str = Field(default='sqlite:///' + sqlite_file)
+    resources_folder: str = ''
+    proxy: str = ''
+    api_key: str = ''
+    gopeed_url: str = ''
+
+class Settings:
+    def __init__(self, settings_file_path: Path = settings_file):
+        self.settings_file_path = settings_file_path
+        self.json_data : None | SettingsJsonModel = None
+
+    async def read_from_file(self):
+        if not self.settings_file_path.exists():
+            raise FileNotFoundError()
+        async with await open_file(self.settings_file_path, 'r', encoding='utf-8') as f:
+            try:
+                contents = await json.loads(await f.read())
+            except:
+                raise SettingsJsonFileInvalideException(f"settings.json file content is invalied, see: {self.settings_file_path}")
+            try:
+                self.json_data = SettingsJsonModel(**contents)
+            except Exception as e:
+                raise SettingsJsonFileInvalideException(f"settings.json file content is invalied, pydantic error info:\n{str(e)}")
+            return self.json_data
+
+    async def get_json_settings_data(self):
+        if self.json_data == None:
+            await self.read_from_file()
+        return self.json_data
+    
+    async def create_template_settings_json(self):
+        async with await open_file(self.settings_file_path, 'w', encoding='utf-8') as f:
+            await f.write(SettingsJsonModel().model_dump_json(indent=2))
+
+    async def save_to_file(self):
+        async with await open_file(self.settings_file_path, 'w', encoding='utf-8') as f:
+            contents = await f.write(self.json_data.model_dump_json(indent=2))
+    
+    async def check_gopeed_service(self):
+        response = httpx.get(urljoin(settings.gopeed_url, '/api/v1/info'))
+        if response.status_code != 200:
+            raise GopeedServiceNotWorking(msg="gopeed service not working")
+        
+    async def check_settings(self):
+        settings_data = await self.get_json_settings_data()
+        # check if lora_folder exists
+        if not exists(settings_data.resources_folder):
+            raise ModelsFolderNotFound(msg="lora folder doesn't exists")
+
+        # check if gopeed_url exists
+        if not settings_data.gopeed_url:
+            raise GopeedServiceNotFound(msg="gopeed service url not found")
+        
+        # check if gopeed service is working
+        response = httpx.get(urljoin(settings_data.gopeed_url, '/api/v1/info'))
+        if response.status_code != 200:
+            raise GopeedServiceNotWorking(msg="gopeed service not working")
+        
+        # check database
+        # if not exists(settings.db_uri):
+        #     raise DatabaseNotFound(msg="database doesn't exists")
+
+_settings = Settings()
+
+def get_settings_dep():
+    return _settings
+
+SettingsDep = Annotated[Settings, Depends(get_settings_dep)]
+
+async_httpx_client: None | httpx.AsyncClient = None
+
+async def get_async_httpx_client(settings: SettingsDep):
+    global async_httpx_client
+    if async_httpx_client == None:
+        settings_data = await settings.get_json_settings_data()
+        async_httpx_client == httpx.AsyncClient(proxy=settings_data.proxy)
+        return async_httpx_client
+    else:
+        return async_httpx_client
+
+AsyncHttpxClientDep = Annotated[httpx.AsyncClient, Depends(get_async_httpx_client)]
+
+httpx_client: None | httpx.Client = None
+
+async def get_httpx_client(settings: SettingsDep):
+    global httpx_client
+    if httpx_client == None:
+        settings_data = await settings.get_json_settings_data()
+        async_httpx_client == httpx.Client(proxy=settings_data.proxy)
+        return httpx_client
+    else:
+        return httpx_client
+    
+HttpxClientDep = Annotated[httpx.Client, Depends(get_httpx_client)]
+
+gopeed_client: None | GospeedAPI = None
+
+async def get_gopeed_client(settings: SettingsDep):
+    global gopeed_client
+    if gopeed_client == None:
+        settings_data = await settings.get_json_settings_data()
+        gopeed_client = GospeedAPI(gopeed_host=settings_data.gopeed_url)
+        return gopeed_client
+    else:
+        return gopeed_client
+
+GopeedClientDep = Annotated[GospeedAPI, Depends()]
+
+engine: None | Engine = None
+
+async def get_db_engine(settings: SettingsDep):
+    global engine
+    if engine == None:
+        settings_data = await settings.get_json_settings_data()
+        engine = create_engine(settings_data.db_uri)
+        SQLModel.metadata.create_all(engine)
+        return engine
+    else:
+        return engine
+
+DbEngineDep = Annotated[Engine, Depends(get_db_engine)]
 
 @contextmanager
-def get_db_session():
+async def get_db_session(engine: DbEngineDep):
     session = Session(engine)
     try:
         yield session
@@ -19,4 +224,3 @@ def get_db_session():
         session.close()
 
 DbSessionDep = Annotated[Session, Depends(get_db_session)]
-
