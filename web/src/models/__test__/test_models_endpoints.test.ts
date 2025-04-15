e7064dfd786fbb64f36d9e9c8ccb950227e118ae
Pyon~ Pyon~~ <32931796+tick97115115@users.noreply.github.com>
2025-04-15 15:51:06 +0800

    update

---
+++ web/src/models/__test__/test_models_endpoints.test.ts
@@ -0,0 +1,5 @@
+import { describe, test, expect } from "vitest";
+
+test("test", () => {
+  expect(true).eq(true);
+});
---
+++ web/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, mergeConfig, configDefaults } from "vitest/config";
+import { fileURLToPath } from "node:url";
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+    },
+  })
+);
