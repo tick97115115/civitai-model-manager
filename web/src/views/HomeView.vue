384a5735af272229bdb72ff661272fffefa1e8ce
2860996570@qq,com <2860996570@qq.com>
2025-04-15 10:57:49 +0800

    new web vue3

---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+</template>
+<script setup lang="ts">
+
+import { useDark, useToggle } from '@vueuse/core'
+
+</script>
